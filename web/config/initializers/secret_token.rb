ca5b4f6e23b3745d23ecf666c78c93251fb5b1e4
halfbeat <adejes@gmail.com>
2012-03-10 21:49:18 +0100

    Import web Pani

---
+++ web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = '31e6588f1478532f3b8d52675d1443370695936943bae711e7989067ba28025c66c1950cdf1046d4074754617a9377df38ad934b462eed590b5764c7171557d8'
